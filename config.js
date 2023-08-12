295ad6506ecd5e809fe588288fc7eb2e6b7b4c1e
fzf404 <me@fzf404.art>
2023-08-12 22:06:09 +0800

    feat: close import position

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 /** @type {import('stylelint').Config} */
 module.exports = {
   extends: ['stylelint-config-standard-scss', 'stylelint-prettier/recommended'],
+  rules: {
+    'no-invalid-position-at-import-rule': null,
+  },
 }
